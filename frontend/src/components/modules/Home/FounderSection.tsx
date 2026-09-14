d2fd47e86aae14fd051dac0ad42903fbea99071f
unknown <najimuddinhelalal670@gmail.com>
2026-09-14 22:47:20 +0600

    added navigation file

--- frontend/src/components/modules/Home/FounderSection.tsx
+++ frontend/src/components/modules/Home/FounderSection.tsx
@@ -1,7 +1,7 @@
+import { Link } from "@/i18n/navigation";
 import { ArrowRight } from "lucide-react";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function FounderSection() {
   const t = useTranslations("FounderSection.Section");
